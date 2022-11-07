50772f09725116b4c8b157da2898d2964f111915
STD\s20217061 <20217061-SasakiNami@hcs.ac.jp>
2022-11-07 15:06:48 +0900

    メイン画面のボタン配置、フォーム2の新規作成

--- Rail_Record_System/Rail_Record_System/Form1.Designer.cs
+++ Rail_Record_System/Rail_Record_System/Form1.Designer.cs
@@ -29,13 +29,63 @@ namespace Rail_Record_System
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(114, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(361, 284);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "乗車記録\r\n確認・修正・削除";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("MS UI Gothic", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button2.Location = new System.Drawing.Point(510, 104);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(361, 284);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "乗車記録\r\n新規登録";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("MS UI Gothic", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button3.Location = new System.Drawing.Point(327, 421);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(331, 125);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "アプリ終了";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(982, 653);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximumSize = new System.Drawing.Size(1000, 700);
+            this.MinimumSize = new System.Drawing.Size(1000, 700);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Rail_Record_System/Rail_Record_System/Form2.cs
+++ Rail_Record_System/Rail_Record_System/Form2.cs
@@ -10,16 +10,11 @@ using System.Windows.Forms;
 
 namespace Rail_Record_System
 {
-    public partial class Form1 : Form
+    public partial class Form2 : Form
     {
-        public Form1()
+        public Form2()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
